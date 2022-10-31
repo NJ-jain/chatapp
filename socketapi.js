7bb9861cb83b78d42981150c19ba26d07d56860c
naman <nn.jain5272@gmail.com>
2022-10-31 17:08:07 +0530

    first commit

---
+++ socketapi.js
@@ -0,0 +1,48 @@
+const io = require( "socket.io" )();
+var user = [];
+var userid = [];
+const socketapi = {
+    io: io
+};
+
+
+// Add your socket.io logic here!
+io.on( "connection", function( socket ) {
+    // console.log( user[userid.indexOf(socket.id)] +" connected" );
+   
+
+    socket.on('disconnect', () => {
+        // console.log( user[userid.indexOf(socket.id)] +" disconnected" );
+        user.splice(userid.indexOf(socket.id) , 1);
+        userid.splice(userid.indexOf(socket.id), 1);
+        console.log(user[userid.indexOf(socket.id)] +  " disconnected" );
+
+     });
+     socket.on('msg' , function(message){
+        console.log(message);
+        io.emit('msg' , {message , username : user[userid.indexOf(socket.id)]});
+
+     })
+
+
+     socket.on("private message", (msg) => {
+
+         socket.broadcast.emit('private message' , {msg , username : user[userid.indexOf(socket.id)]});
+      });
+     socket.on('naam' , function(naam){
+        user.push(naam); 
+        userid.push(socket.id);
+        console.log( user[userid.indexOf(socket.id)] +" connected" );
+        io.emit('online' , {data : user});
+    })
+
+    
+
+
+});
+// end of socket.io logic
+
+module.exports = socketapi;
+
+
+
